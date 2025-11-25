0aad6e9d1f9012f458d686f6d16c293bda79fbcc
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-11-25 14:35:53 +0500

    Update 1_2.cpp

--- sem_1/1_2/1_2.cpp
+++ sem_1/1_2/1_2.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+main(){
+    float a, b; 
+    cin >> a >> b; 
+    if(b == 0){
+        cout << "Ne nyzno!" << endl; 
+    }
+    else{ 
+        cout << a/b << endl;
+    }
+}
