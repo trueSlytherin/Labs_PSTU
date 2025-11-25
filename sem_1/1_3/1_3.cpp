cceb8cdf0ee42827dace1bad9c9608878b3bd979
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-11-25 14:36:26 +0500

    Update 1_3.cpp

--- sem_1/1_3/1_3.cpp
+++ sem_1/1_3/1_3.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+using namespace std;
+main(){
+    float a, b; 
+    cin >> a; 
+    if (a < 5){
+        b = a * 3;
+    }
+    
+    if ( a > 7){
+        b = a + 7; 
+    }
+    else{
+        b = a / 10;
+    }
+    cout << b << endl; 
+}
