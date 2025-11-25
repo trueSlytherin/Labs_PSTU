7cdfd90dd80fad9d2649da85d1ad12ca012b3926
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-11-25 15:24:50 +0500

    Update 2_2.cpp

--- sem_1/2_2/2_2.cpp
+++ sem_1/2_2/2_2.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+main(){
+    int N; int b = 1; 
+    cin >> N; 
+    if (N <= 0){
+        cout << "Нельзя посчитать произведение натуральных чисел при N <= 0" << endl;
+    }
+    else{
+        for (int i = 1; i <= N; i ++){
+        b *= i;
+        }
+    }
+    cout << b << endl; 
+}
