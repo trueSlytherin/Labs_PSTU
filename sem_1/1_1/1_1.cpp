e47434bf4aee23c1a34fd733a634ec2d8b2ade74
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-11-25 14:25:43 +0500

    Update 1_1.cpp

--- sem_1/1_1/1_1.cpp
+++ sem_1/1_1/1_1.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+main(){
+    float a, b; 
+    cin >> a >> b; 
+    if(b == 0){
+        cout << "Ne nado!!!" << endl; 
+    }
+    else{ 
+        cout << a/b << endl;
+    }
+}
