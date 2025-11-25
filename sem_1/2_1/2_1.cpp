dbd983280d248c6f4ae9aab8fdf48fd3fe47bcd5
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-11-25 15:23:42 +0500

    Update 2_1.cpp

--- sem_1/2_1/2_1.cpp
+++ sem_1/2_1/2_1.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+main(){
+    int N, b; 
+    cin >> N; 
+    if (N == 0){
+        cout << "Нельзя посчитать сумму натуральных чисел при N <= 0" << endl;
+    }
+    while (N!=0){
+        b += N;
+        N -= 1;
+    }
+    cout << b << endl; 
+}
