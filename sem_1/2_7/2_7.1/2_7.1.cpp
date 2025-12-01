da0aee20989a310262a31c7c211b297598b9e029
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-01 18:40:33 +0500

    Update 2_7.1.cpp

--- sem_1/2_7/2_7.1/2_7.1.cpp
+++ sem_1/2_7/2_7.1/2_7.1.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <cmath> 
+using namespace std;
+int main(){
+    int N, k; 
+    cin >> N; 
+    k = sqrt(N); 
+    for(int i = 0; i < k; i++){ 
+        for (int j = 0; j < k; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
