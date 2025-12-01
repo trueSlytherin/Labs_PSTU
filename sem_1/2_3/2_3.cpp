fec66862cb037c286ee70d6e95146c45ab7f929d
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-01 18:17:58 +0500

    Update 2_3.cpp

--- sem_1/2_3/2_3.cpp
+++ sem_1/2_3/2_3.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream> 
+using namespace std;
+int main(){
+    int S, N, a, b; 
+    cin >> N; 
+    while (N != 0){
+        b = 1;
+        a = 2*N;
+        while (a >= N){
+            b = b * a; 
+            a -= 1; 
+        }
+        S = S + b;
+        N = N - 1; 
+    }
+    cout << S << endl; 
+    return 0; 
+}
+
+int n; 
+int t, s = 0; 
