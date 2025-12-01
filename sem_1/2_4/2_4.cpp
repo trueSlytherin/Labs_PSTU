b70f65ea2f2c9cee802eaea4bc95121962debcf0
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-01 18:31:53 +0500

    Update 2_4.cpp

--- sem_1/2_4/2_4.cpp
+++ sem_1/2_4/2_4.cpp
@@ -1,1 +1,24 @@
-
+#include <iostream> 
+using namespace std;
+int main(){
+    int N; 
+    cin >> N; 
+    int sp = N/2; 
+    int st = 1; 
+    if (N%2 == 0, N<3){
+        cout << "Нельзя вводить четное число и число, меньшее 3" << endl; 
+    }
+    else{
+        for (int i = 0; i < (N+1)/2; i++){
+        for (int j = 0; j < sp; j++){
+            cout << " "; 
+        }
+        sp --; 
+        for (int j = 0; j < st; j++){
+            cout << "*";
+        }
+        st += 2; 
+        cout << endl;
+        }
+    }
+}
