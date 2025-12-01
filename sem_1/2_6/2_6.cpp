33f06aa7660302152aec8e7bbe6ceab0fa1ef411
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-01 18:33:39 +0500

    Update 2_6.cpp

--- sem_1/2_6/2_6.cpp
+++ sem_1/2_6/2_6.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream> 
+using namespace std;
+int main(){
+    int N; 
+    cin >> N; 
+    if (N <= 2){
+        cout << "Введите число больше 2" << endl;
+    }
+    else{
+        for (int i = 0; i < N; i++){
+            for (int j = 1; j <= i; j++){
+                cout << " "; 
+            }
+            for (int j = 0; j < N - i; j++){
+                cout << "*"; 
+            }
+            cout << endl;
+        }
+    }
+}
