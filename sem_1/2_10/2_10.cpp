5cab5be1561278577584e73107b3a73127bea785
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-08 12:50:43 +0500

    Update 2_10.cpp

--- sem_1/2_10/2_10.cpp
+++ sem_1/2_10/2_10.cpp
@@ -1,1 +1,15 @@
+int n, tmp, max;
+
+cout << "Введите длину послед:" << endl;
+cin >> n;
+
+cout << "Введите первое число:" << endl;
+cin >> n;
+for (int i = 2; i <= n; i++)
+{
+	cout << "Введите след число:" << endl;
+	cin >> tmp;
+	if (tmo > max) { max = tmp; }
+}
+cout <<"максимальный элемент: " << max << endl;
 
