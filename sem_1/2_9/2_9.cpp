19afab54465d180def74dadbaf03a83d0d12feac
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-08 12:20:38 +0500

    Update 2_9.cpp

--- sem_1/2_9/2_9.cpp
+++ sem_1/2_9/2_9.cpp
@@ -1,1 +1,88 @@
 
+
+int n, d;
+
+cin; >> n;
+d= n / 2;
+// задняя
+for (int i = 0; i < d; i++)
+{
+	cout << "";
+}
+for (int i = 0; i < n; i++)
+{
+	cout << '*';
+}
+cout << endl;
+// верх диог
+for (int i = 0; i < d - 1; i++)
+{
+	for (int j = 1; j < d - 1; j++)
+	{
+		cout << "";
+
+	}
+	cout << '*';
+	for (int j = 0; j < n - 2; j++)
+	{
+		cout << "";
+	}
+	cout << '*';
+	for (int j = 1; j < 1; j++)
+	{
+		cout << "";
+	}
+	cout << '*' << endl
+}
+// перелняя верх плюс кус задн
+for (int i = 0; i < n; i++)
+{
+	cout << '*';
+}
+for (int i = 0; i < d - 2; i++)
+{
+	cout << "";
+}
+cout << '*' << endl;
+//боковые грани
+for (int i = 0; i < d - 1; i++)
+{
+	cout << '*';
+	for (int j = 0; j < n - 2; j++)
+	{
+		cout << "";
+	}
+	cout << '*';
+	for (int j = 1; j < d - i - 1; j++)
+	{
+		cout << "";
+	}
+	cout << '*' << endl
+}
+// нижние диоганали
+for (int i = 0; i < d - 1; i++)
+{
+	cout << '*';
+	for (int j = 0; j < n - 2; j++)
+	{
+		cout << "";
+	}
+	cout << '*';
+	for (int j = 1; j < d - i - 1; j++)
+	{
+		cout << "";
+	}
+	cout << '*' << endl;
+}
+// нижняя грань
+for (int i = 0; i < n; i++)
+{
+	cout << '*';
+}
+cout << endl;
+
+
+
+
+
+
