dcdec58097dd9cb53e75dc189bfc8ae6d6c3dd69
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-09 13:49:44 +0500

    Update 2_20.cpp

--- sem_1/2_20/2_20.cpp
+++ sem_1/2_20/2_20.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream> 
+#include <cmath>
+using namespace std; 
+int main(){
+    int N, S, a; 
+    bool flag = false;
+    cin >> N >> S; 
+    for(int i = 1; i <= N; i++){
+        a = sin(N + (i/N));
+        if (a == S){
+            flag == true; 
+            cout << "Да, число существует в последовательности" << endl; 
+        }
+    }
+    if (flag == false){
+        cout << "Нет, числа не существует в последовательности" << endl; 
+    }
+    return 0;
+}
