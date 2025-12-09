8aab3c4b4b77eea0b5a2c2b5e746b0f30507dedb
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-09 13:50:34 +0500

    Update 2_19.cpp

--- sem_1/2_19/2_19.cpp
+++ sem_1/2_19/2_19.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream> 
+using namespace std; 
+int main(){
+    int N, S; 
+    cout << "Введите количество слагаемых "; 
+    cin >> N; 
+    for (int i = 1; i <= N; i++){
+        if (i%3== 0){
+            S-=i; 
+        } 
+        else{
+            S+=i;
+        } 
+    }
+    cout << "S = " << S << endl; 
+} 
