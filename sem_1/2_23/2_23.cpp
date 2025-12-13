078371c807e4f8861c3de3d6417fd2b07d37bed6
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 16:12:17 +0500

    Update 2_23.cpp

--- sem_1/2_23/2_23.cpp
+++ sem_1/2_23/2_23.cpp
@@ -1,1 +1,23 @@
-
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n, number=0;
+    double nmax = -1;
+    bool flag = false;
+    cin >> n;
+    for (float i = 1; i <= n; i++) {
+        double a = sin(n + (i / n));
+        if (nmax < a) {
+            nmax = a;
+        }
+    }
+    for (float i = 1; i <= n; i++) {
+        double a = sin(n + (i / n));
+        if (abs(nmax - a) < 0.000001) { number += 1; }
+    }
+    cout <<endl<< number<<nmax;
+}
