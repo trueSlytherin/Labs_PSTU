1eb23e669c0ea469afee09d080792f38f2418fa0
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 16:07:53 +0500

    Update 2_.cpp

--- sem_1/2_5/2_.cpp
+++ sem_1/2_5/2_.cpp
@@ -1,15 +1,21 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-int main(){
-    int N; 
-    cin >> N; 
-    for (int i = 0; i < N; i++){
-        for (int j = 1; j < N-i; j++){
-            cout << " "; 
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i; j++)
+        {
+            cout << " ";
         }
-        for (int j = 0; j <= i; j++){
+        for (int j = 0; j <= i; j++)
+        {
             cout << "*";
         }
         cout << endl;
     }
+
+    return 0;
 }
