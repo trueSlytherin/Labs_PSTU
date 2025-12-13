c7b3d2ce555e530e889b12fab21f6108d52830c7
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 16:12:01 +0500

    Update 2_22.cpp

--- sem_1/2_22/2_22.cpp
+++ sem_1/2_22/2_22.cpp
@@ -1,1 +1,20 @@
-
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n,number;
+    double nmax=-1;
+    bool flag = false;
+    cin >> n;
+    for (float i = 1; i <= n; i++) {
+        double a = sin(n + (i / n));
+        if (nmax < a) {
+            nmax = a;
+            number = i;
+        }
+    }
+    cout <<"Максимальное число: " << nmax << endl << "Номер: " << number;
+}
