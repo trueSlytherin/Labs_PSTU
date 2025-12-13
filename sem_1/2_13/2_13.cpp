db5182b43b8c6cfc30467e60951d21da2e63f628
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 15:21:06 +0500

    Update 2_13.cpp

--- sem_1/2_13/2_13.cpp
+++ sem_1/2_13/2_13.cpp
@@ -6,13 +6,13 @@ int main(){
     cin >> a >> b >> c; 
     D = pow(b, 2) - 4*a*c;
     if (D > 0){
-        x = ((-b) + sqrt(D)) / (2*a); 
-        x1 = ((-b) - sqrt(D)) / (2*a);
+        x1 = ((-b) + sqrt(D)) / (2*a); 
+        x2 = ((-b) - sqrt(D)) / (2*a);
         cout << "x1 =" << x << "x2 = " << x1 << endl;
     }
     else if (D == 0){
         x = (-b) / (2*a);
-        cout << "x= " << x << endl;
+        cout << "x1= " << x << endl;
     }
     else {cout << "Корней нет" << endl;}
     return 0;
