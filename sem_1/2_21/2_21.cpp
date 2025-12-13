0735714b764878736c552c295a3ef29d52db526a
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 16:11:47 +0500

    Update 2_21.cpp

--- sem_1/2_21/2_21.cpp
+++ sem_1/2_21/2_21.cpp
@@ -1,1 +1,17 @@
-
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    bool flag = true;
+    int n1,n2;
+    cin >> n1;
+    while (n1 != 0) {
+        cin >> n2;
+        if (n1 > n2 and n2!=0) { flag = false; }
+        n1 = n2;
+    }
+    if (flag==true) { cout << "Числа упорядочены по возрастанию"; }
+    else { cout << "Числа не упоряжочены по возрастанию"; }
+}
