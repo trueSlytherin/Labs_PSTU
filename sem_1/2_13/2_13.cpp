069bd12997ecf5dd09b22c64e58bac30e7948509
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 15:10:03 +0500

    Update 2_13.cpp

--- sem_1/2_13/2_13.cpp
+++ sem_1/2_13/2_13.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 #include <cmath>
-int main()
-{
-	int a, b, c; 
-	float D, x1, x2;
-	std::cout << "enter coefficient a: ";
-	std::cin >> a;
-	std::cout << "enter coefficient b: ";
-	std::cin >> b;
-	std::cout << "enter coefficient c: ";
-	std::cin >> c;
-	D = b * b - 4 * a * c;
-	if (D == 0)
-	{
-		std::cout << "x = " << (-1 * b) / (2 * a);
-	}
-	else if (D > 0)
-	{
-		std::cout << "x1 = " << ((-1 * b) + sqrt(D)) / (2 * a) << std::endl;
-		std::cout << "x2 = " << ((-1 * b) - sqrt(D)) / (2 * a);
-	}
-	else
-	{
-		std::cout << "D < 0";
-	}
-	return 0;
-}
+using namespace std;
+int main(){
+    float a, b, c, x, x1, D;
+    cin >> a >> b >> c; 
+    D = pow(b, 2) - 4*a*c;
+    if (D > 0){
+        x = ((-b) + sqrt(D)) / (2*a); 
+        x1 = ((-b) - sqrt(D)) / (2*a);
+        cout << "x1 =" << x << "x2 = " << x1 << endl;
+    }
+    else if (D == 0){
+        x = (-b) / (2*a);
+        cout << "x= " << x << endl;
+    }
+    else {cout << "Корней нет" << endl;}
+    return 0;
+    }
