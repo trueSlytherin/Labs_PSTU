8b45ec34374db6e0d535852ea9ccb3bdf2a67852
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-13 15:25:03 +0500

    Update 2_12.cpp

--- sem_1/2_12/2_12.cpp
+++ sem_1/2_12/2_12.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
-int main()
-{
-	int N, a, max = 0, min;
-	std::cin >> N;
-	for (int i = 1; i <= N; i++)
-	{
-		std::cin >> a;
-		if (i == 1)
-		{
-			min = a;
-		}
-		if (a > max)
-		{
-			max = a;
-		}
-		if (a < min)
-		{
-			min = a;
-		}
-	}
-	std::cout << "sum = " << max + min;
-	return 0;
+using namespace std;
+int main(){
+    int N, max, min, tmp;
+    cout << "Введите количество элементов:"; 
+    cin >> N; 
+    cin >> min;
+    max = min;
+    for (int i=2; i <= N; i++){
+        cin >> tmp;
+        if (max < tmp){ max = tmp; }
+        if (min > tmp){ min = tmp; } 
+    }
+    cout << "Сумма максимального и минимального члена последовательности: " << max + min << endl;
+    return 0;
 }
