f3ca86e9a78062f93c6313d2a1f2c1e9cb117cf9
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-23 03:57:40 +0500

    Update ptr_5.cpp

--- sem_1/ptr_5/ptr_5.cpp
+++ sem_1/ptr_5/ptr_5.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, f = 1;
+	int *ptr_n = &n;
+	int *ptr_f = &f;
 
+	cin >> *ptr_n;
+
+	for (size_t i = 1; i <= *ptr_n; i++) {
+		*ptr_f *= i;
+	}
+	cout << f;
+}
