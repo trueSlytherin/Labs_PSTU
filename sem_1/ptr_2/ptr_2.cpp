72d50d08c0d50e5664164be5072fdfcaf8e63fff
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-23 03:56:36 +0500

    Update ptr_2.cpp

--- sem_1/ptr_2/ptr_2.cpp
+++ sem_1/ptr_2/ptr_2.cpp
@@ -1,1 +1,17 @@
+#include <iostream> 
+using namespace std; 
+int main(){
+    int N, S;
+    cout << "Введите первое число: "; 
+    cin >> N;
+    cout << "Введите второе число: "; 
+    cin >> S;
+    int *ptr1 = &N; 
+    int *ptr2 = &S; 
+    int tmp = *ptr1; 
+    *ptr1 = *ptr2; 
+    *ptr2 = tmp;
+    cout << N << S << endl;
 
+    return 0;
+}
