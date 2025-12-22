98d6a6fa31acf2e4a591ee6ee4c683a1487e2314
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-23 03:57:21 +0500

    Update ptr_4.cpp

--- sem_1/ptr_4/ptr_4.cpp
+++ sem_1/ptr_4/ptr_4.cpp
@@ -1,1 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, mx, mn;
+    cin >> n;
 
+    int* ptr1 = &mx;
+    int* ptr2 = &mn;
+
+    //ввод первого числа 
+    int c;
+    cin >> c;
+
+    *ptr1 = c;
+    *ptr2 = c;
+    
+    for (int i = 2; i <= n; i++) {
+        //ввод второго числа
+        cin >> c;
+    }
+        if (c < *ptr2) { *ptr2 = c; }
+        if (c > *ptr1) { *ptr1 = c; }
+
+        cout << "max:" << mx << endl;
+        cout << "min:" << mn << endl;
+    
+    return 0;
+}
