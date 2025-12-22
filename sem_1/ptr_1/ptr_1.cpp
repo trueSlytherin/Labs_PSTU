ea7d48f56528c20d323da3080b9536e38c81654f
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-23 03:56:10 +0500

    Update ptr_1.cpp

--- sem_1/ptr_1/ptr_1.cpp
+++ sem_1/ptr_1/ptr_1.cpp
@@ -1,1 +1,22 @@
+﻿#include <iostream>
+using namespace std;
 
+
+int main()
+{
+    int x, i;
+    cin >> x;
+    int *ptr = &x;
+    cout << x << endl;
+
+    cin >> i;
+
+    *ptr = i;
+    cout << x << endl;
+
+    *ptr += 5;
+    cout << x << endl;
+
+    return 0;
+
+}
