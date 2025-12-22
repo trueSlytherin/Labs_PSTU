0f1ecf4ad13e64d8e337007829c51159586cd980
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-23 03:14:59 +0500

    Update eq_newton.cpp

--- sem_1/eq_newton/eq_newton.cpp
+++ sem_1/eq_newton/eq_newton.cpp
@@ -1,1 +1,23 @@
-
+//метод ньютона или касательной
+//f(x)= arccos(x) - /1- 0.3x**3 [0, 1]
+//точность E = 0,000001
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    float x, xPrev;
+    double eps = 0.000001;
+    //проверка что идет сначала а или б просто подставить сначала а в функцию и посчитать а потом б
+    //f(b) = 3,14 * 0,84 > 0 тоесть икс нулевая будет Б(еслиб было меньше то взять а подставить в функцию и т.д)
+    // f(a) = 1, 57 - 1 > 0 
+    // (acos(a) - sqrt(1 - 0.3 * pow(a, 3)))
+    x = 0;
+    xPrev = 1;
+    while(abs(x-xPrev)>eps)
+    {
+        xPrev = x;
+        x = xPrev - (acos(xPrev) - sqrt(1 - 0.3 * pow(xPrev, 3))) / (((-1)/sqrt(1-pow(xPrev,2))) + ((0.45*pow(xPrev, 2))/sqrt(1- 0.3* pow(xPrev,3))));
+    }
+    cout << "Корень: " << x << endl;
+    return 0;
+}
