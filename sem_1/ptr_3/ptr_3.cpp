53e4bf9f203f37b6d32898ee7511f32362656173
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2025-12-23 03:56:54 +0500

    Update ptr_3.cpp

--- sem_1/ptr_3/ptr_3.cpp
+++ sem_1/ptr_3/ptr_3.cpp
@@ -1,1 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float a = 3.14f, b = 2.17f, sum;
+    float* ptr_a = &a;
+    float* ptr_b = &b;
+    sum = *ptr_a + *ptr_b;
 
+    cout << a <<" "<< b << endl;
+    cout << "a" << "+" << "b" << "=" << sum << endl;
+
+    return 0;
+}
