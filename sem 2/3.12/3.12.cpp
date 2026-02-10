19fa4e3de891e50168725d6feefec355840aadf0
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 02:37:04 +0500

    Update 3.12.cpp

--- sem 2/3.12/3.12.cpp
+++ sem 2/3.12/3.12.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int n = 3;
+    int k = 1;
+
+    int arr[n][n] = { {1, 1, 1},{2, 1, 1},{1, 2, 1} };
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j + k) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
