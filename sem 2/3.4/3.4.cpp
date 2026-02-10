9d7b34b6128d75db183963e840a7510d19f86153
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 01:40:38 +0500

    Update 3.4.cpp

--- sem 2/3.4/3.4.cpp
+++ sem 2/3.4/3.4.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    const int n = 6;
+
+    int arr[n] = { 5, 2, 1, 3, 5, 7 };
+
+    bool sorted = true;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            sorted = false;
+            break; // раннее прерывание
+        }
+    }
+
+    if (sorted)
+    {
+        cout << "Массив отсортирован" << endl;
+    }
+    else
+    {
+        cout << "Массив не упорядочен по возрастанию" << endl;
+    }
+
+    return 0;
+}
