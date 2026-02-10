b9583789440deb8b78ea712513f4c186c6c937b5
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 01:45:25 +0500

    Update 3.5.cpp

--- sem 2/3.5/3.5.cpp
+++ sem 2/3.5/3.5.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int n = 10;
+    int arr[n] = { 12, 967, 53, 10, 62, 63, 56, 919, 1, 158 };
+
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << max << endl;
+
+    return 0;
+}
