9e71df0c9263f73c6fb2d3a97ab5df705450d4ed
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 02:24:59 +0500

    Update 3.11.cpp

--- sem 2/3.11/3.11.cpp
+++ sem 2/3.11/3.11.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int n = 3;
+
+    int arr[n][n] = { {1, 5, 0}, {5, 1, 5}, {0, 5, 1} };
+
+    // обнуляем главную диагональ
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    // вывод матрицы
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
