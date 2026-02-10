e026868851165857fece584c224f46f5e7202974
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 02:14:44 +0500

    Update 3.9.cpp

--- sem 2/3.9/3.9.cpp
+++ sem 2/3.9/3.9.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int n = 10;
+    int arr[n] = { 99, 22, 7546, 1, 547, 848, 123, 425, 6234, 63 };
+    int arr_2[n];
+    int j = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            arr_2[j++] = arr[i];
+        }
+    }
+
+    for (int i = 0; i < j; i++) {
+        cout << arr_2[i] << " ";
+    }
+
+    return 0;
+}
