e2d8178be02e100a13a6bf8a950f2e363cf48361
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 01:38:15 +0500

    Update 3.3.cpp

--- sem 2/3.3/3.3.cpp
+++ sem 2/3.3/3.3.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    const int n = 11;
+    int max;
+    int count = 1;
+
+    int arr[n] = { 487, 452, 6116, 87, 82, 68, 79, 124, 455, 3545, 265 };
+
+    max = arr[0];
+    cout << arr[0] << " ";
+
+    for (int i = 1; i < n; i++)
+    {
+        cout << arr[i] << " ";
+
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            count = 1;
+        }
+        else if (arr[i] == max)
+        {
+            count++;
+        }
+    }
+
+    cout << endl;
+
+    cout << "Max: " << max << endl;
+    cout << "Количество элементов с этим значением: " << count << endl;
+
+    return 0;
+}
