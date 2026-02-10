b3c9522bb0f8368329be71a3538597a3444d239a
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-10 23:54:59 +0500

    Update 3.1.cpp

--- sem 2/3.1/3.1.cpp
+++ sem 2/3.1/3.1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    const int n = 4;
+    int tmp;
+
+    // массив
+    int arr[n] = { 43, 564, 76, 123 };
+
+    // вывод исходного массива
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // переворачиваем массив
+    for (int i = 0; i < n / 2; i++)
+    {
+        tmp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = tmp;
+    }
+
+    // вывод перевёрнутого массива
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
