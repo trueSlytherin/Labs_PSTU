d7c90d06c653bb5b762d1f56bd4129a8624f64db
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 01:59:01 +0500

    Update 3.7.cpp

--- sem 2/3.7/3.7.cpp
+++ sem 2/3.7/3.7.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int n = 10;
+    int arr[n] = { 263, 1432, 5, 62, 72, 96, 1346, 5367, 6, 1 };
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        else if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << max << endl;
+    cout << min << endl;
+
+    return 0;
+}
