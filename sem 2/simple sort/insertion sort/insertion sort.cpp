113490b7bb098ba771fba2fcaf90d128dc7b3105
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-11 03:09:10 +0500

    Update insertion sort.cpp

--- sem 2/simple sort/insertion sort/insertion sort.cpp
+++ sem 2/simple sort/insertion sort/insertion sort.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    const int n = 7;
+    int tmp, min_index;
+
+    int arr[n] = { 9, 3, 5, 6, 11, 21, 1 };
+
+    // вывод исходного массива
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // сортировка выбором
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+
+        tmp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = tmp;
+    }
+
+    // вывод отсортированного массива
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
