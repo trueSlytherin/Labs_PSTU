f9eba1d6b45d2ecf55738bc3a1311c57ec8e3d95
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-17 23:32:52 +0500

    Update hanoi.cpp

--- sem 2/hanoi tower/hanoi.cpp
+++ sem 2/hanoi tower/hanoi.cpp
@@ -1,1 +1,16 @@
+﻿#include <iostream>
+using namespace std;
 
+void hanoi(int n, int start, int point, int temp) {
+	if (n <= 0) return;
+	hanoi(n - 1, start, temp, point);
+	cout << start << " => " << point << endl;
+	hanoi(n - 1, temp, point, start);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	hanoi(n, 1, 3, 2);
+	return 0;
+}
