a6044f4d5aea8562e126819b463aa3d464c22a73
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-17 23:33:41 +0500

    Update fib.cpp

--- sem 2/fibonachi/fib.cpp
+++ sem 2/fibonachi/fib.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int fib(int n)
+{
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+    return fib(n - 2) + fib(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+        cout << fib(i)<<" ";
+    return 0;
+    
+}
