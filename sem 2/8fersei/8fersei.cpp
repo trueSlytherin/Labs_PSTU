d8285cf6e6dd5792d1901b6bc965c9d87ab287ae
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-02-24 19:46:25 +0500

    Update 8fersei.cpp

--- sem 2/8fersei/8fersei.cpp
+++ sem 2/8fersei/8fersei.cpp
@@ -1,83 +1,69 @@
-﻿#include <iostream>
-using namespace std;
+#include <iostream>
 
-const int N = 8;
+const int size = 8;
+int board[size][size];
+int count = 0;
 
-void vizualization(int arr[N][N]) {
-
-	cout << "\t";
-	for (int i = 0; i < N; i++) {
-		cout << "  " << i << " ";
-	}
-	// cout << "  columns";
-	cout << endl;
-
-	for (int i = 0; i < N; i++) {
-		cout << "\t";
-		for (int j = 0; j < N; j++) {
-			cout << "----";
-		}
-		cout << "-" << endl;
-		cout << "     " << i << "\t";
-
-		for (int j = 0; j < N; j++) {
-			cout << "| ";
-			if (arr[i][j] == 1) cout << "Q ";
-			else cout << "  ";
-		}
-		cout << "|" << endl;
-	}
-	// cout << "   rows";
-	cout << "\t";
-	for (int j = 0; j < N; j++) {
-		cout << "----";
-	}
-	cout << "-" << endl << endl << endl;
+// вывод доски
+void show() { 
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (board[i][j]) { 
+                std::cout << "# ";
+            }
+            else {
+                std::cout << ". ";
+            }
+        }
+        std::cout << std::endl;
+    }
 }
 
-bool canSetQueen(int arr[N][N], int x, int y) {
-	for (int row = 0; row < y; row++) {
-		for (int col = 0; col < N; col++) {
-			if (arr[row][col] == 1) {
-				if ((x == col) || (y == row) || (x - y == col - row) || ((N - x) - y == (N - col) - row))
-					return 0;
-			}
-		}
-	}
-	return 1;
+// бьется ли ячейка
+int check(int i, int j) {
+    // проверка столбца
+    for (int c = 0; c < i; c++) {
+        if (board[c][j]) {
+            return 0;
+        }
+    }
+
+    // проверка диагонали влево вверх
+    for (int c = 1; c <= i && j - c >= 0; c++) {
+        if (board[i - c][j - c]) {
+            return 0;
+        }
+    }
+
+    // проверка диагонали вправо вверх
+    for (int c = 1; c <= i && j + c < size; c++) {
+        if (board[i - c][j + c]) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
-void eightQueen(int arr[N][N], int row) {
-	static int count = 0;
-	for (int col = 0; col < N; col++) {
-		if (canSetQueen(arr, col, row)) {
-			if (row == N - 1) {
-				arr[row][col] = 1;
-				vizualization(arr);
-				arr[row][col] = 0;
-				count++;
-			}
-			else {
-				arr[row][col] = 1;
-				eightQueen(arr, row + 1);
-				arr[row][col] = 0;
-			}
-		}
-	}
-	if (row == 0) cout << "\ttotal boards: " << count << endl << endl << endl;
+// i - номер строки
+void set(int i) {
+    if (i == size) {
+        std::cout << "Variation " << ++count << ":" << std::endl;
+        show();
+        std::cout << std::endl << std::endl;
+        return;
+    }
+    for (int ii = 0; ii < size; ii++) {
+        // проверка строки
+        if (check(i, ii)) {
+            board[i][ii] = 1;
+            set(i + 1);
+            board[i][ii] = 0; // обнуление доски
+        }
+        // else - откат к предыдущей строке со смещением ферзя
+    }
 }
-
 
 int main() {
-	int arr[N][N];
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			arr[i][j] = 0;
-		}
-	}
-	eightQueen(arr, 0);
-
-
-
-}
+    set(0);
+    return 0;
+} 
