52c86e9662b0e38d2ef55fc7ccb38099b7d8374d
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-03-20 00:15:47 +0500

    Update 3.2.cpp

--- sem 2/3.2/3.2.cpp
+++ sem 2/3.2/3.2.cpp
@@ -1,40 +1,30 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    const int n = 11;
+    const int N = 11;
     int tmp, p, q;
-
-    int arr[n] = { 11, 53, 54, 887, 856, 92, 73, 23, 72, 21, 75 };
-
-    p = 4;
-    q = 7;
-
-    // вывод исходного массива
-    for (int i = 0; i < n; i++)
+    int a[n] = { 11, 53, 54, 887, 856, 92, 73, 23, 72, 21, 75 };
+    cout << "введите p и q" << endl;
+    cin >> p >> q;
+    for (int i = 0; i < N; i++)
     {
-        cout << arr[i] << " ";
+        cout << a[i] << " ";
     }
     cout << endl;
-
-    // переворачиваем участок массива
     while (p < q)
     {
-        tmp = arr[p];
-        arr[p] = arr[q];
-        arr[q] = tmp;
+        tmp = a[p];
+        a[p] = a[q];
+        a[q] = tmp;
         p++;
         q--;
     }
-
-    // вывод результата
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
     {
-        cout << arr[i] << " ";
+        cout << a[i] << " ";
     }
     cout << endl;
-
     return 0;
+}
 }
