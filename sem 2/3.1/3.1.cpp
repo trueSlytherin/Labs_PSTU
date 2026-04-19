4f685fdfbb9f40abe6f8eec42130dc2ed2cc4224
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 02:29:45 +0500

    Update 3.1.cpp

--- sem 2/3.1/3.1.cpp
+++ sem 2/3.1/3.1.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
+int main()
+{
+    const int N = 4;
+    int tmp;
+    int a[N] = { 34, 21, 55, 100 };
+    for (int i = 0; i < N; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < N/2; i++)
+    {
+        tmp = a[i];
+        a[i] = a[N-1-i];
+        a[N-1-i] = tmp;
+    }
 
-int main() {
-	srand(time(0));
-	const int n=10;
-	int a[n];
-	int r_min=0;
-	int r_max=100;
-	for (int i=0; i<n; i++){
-	    a[i]=((double)rand()/RAND_MAX)*(r_max-r_min)+r_min;
-	    cout<<a[i]<<" ";
-	}
-	cout<<endl;
-	int tmp;
-	for (int i=0; i<n/2; i++){
-	    tmp=a[i];
-	    a[i]=a[n-1-i];
-	    a[n-1-i]=tmp;
-	}
-	for (int i=0; i<n; i++){
-	    cout<<a[i]<<" ";
-	}
-	cout<<endl;
-	return 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
