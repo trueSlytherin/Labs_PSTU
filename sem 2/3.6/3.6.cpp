e4b6f80aa3338410ad6189a6aac2769a27b7b8b0
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 03:40:31 +0500

    Update 3.6.cpp

--- sem 2/3.6/3.6.cpp
+++ sem 2/3.6/3.6.cpp
@@ -1,31 +1,17 @@
-#include <iostream> 
-#include <cstdlib>
-#include <ctime>
-using namespace std; 
-int main(){
-    srand(time(0));
-    int range_min = -1000;
-    int range_max = 1000; 
-    int N, k; 
-    cin >> N >> k; 
-    int a[N], b[N];
-    for (int i = 0; i < N; i++)
-    {
-        a[i] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
-        cout << a[i] << " ";
-    }   
-    cout << endl; 
+#include <iostream>
+using namespace std;
 
-    if ( k > N ){ k = k%N;} 
-
-    for (int i = 0; i < N; i++){
-        int K = i - k; 
-        if (K < 0) {K+=N;}
-        b[K] = a[i]; 
-    }    
-
-    for (int i = 0; i < N; i++){
-        a[i] = b[i]; 
-        cout << a[i] << " ";
-    }
+int main() {
+	const int n=7;
+	int a[n]={5,2,4,6,0,1,3};
+	for (int i=0; i<n; i++){
+	    cout<<a[i]<<" ";
+	}
+	cout<<endl;
+	int temp=0;
+	for (int i=0; i<n; i++){
+	    if (a[i]>=temp){temp=a[i];}
+	}
+	cout<<temp;
+	return 0;
 }
