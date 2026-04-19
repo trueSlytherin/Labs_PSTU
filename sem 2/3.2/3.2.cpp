e3a42a52518cfd445708f5503be58892e1ff125e
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 02:31:05 +0500

    Update 3.2.cpp

--- sem 2/3.2/3.2.cpp
+++ sem 2/3.2/3.2.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 int main()
 {
-    const int N = 4;
-    int tmp;
-    int a[N] = { 34, 21, 55, 100 };
+    const int N = 11;
+    int tmp, p, q;
+    int a[N] = { 34, 67, 89, 100, 20, 45, 31, 78, 90, 25, 57 };
+    cout << "введите p и q" << endl;
+    cin >> p >> q;
     for (int i = 0; i < N; i++)
     {
         cout << a[i] << " ";
     }
     cout << endl;
-    for (int i = 0; i < N/2; i++)
+    while (p < q)
     {
-        tmp = a[i];
-        a[i] = a[N-1-i];
-        a[N-1-i] = tmp;
+        tmp = a[p];
+        a[p] = a[q];
+        a[q] = tmp;
+        p++;
+        q--;
     }
-
-
     for (int i = 0; i < N; i++)
     {
         cout << a[i] << " ";
