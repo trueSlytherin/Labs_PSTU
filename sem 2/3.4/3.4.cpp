e038ff749a104708768332283b8be0e5e9a2d038
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 03:54:19 +0500

    Update 3.4.cpp

--- sem 2/3.4/3.4.cpp
+++ sem 2/3.4/3.4.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    const int N = 6;
-    int a[N] = { 1, 5, 3, 2, 10, 6 };
-    bool sorted = true;
-    for (int i = 1; i < N; i++)
-    {
-        if (a[i] < a[i-1])
-        {
-            sorted = false;
-            break; 
+
+int main() {
+    const int n=7;
+    int a[n]={1, 2, 3, 4, 5, 6, 7};
+    bool sorted=true;
+    for (int i=1; i<n; i++){
+        if (a[i]<a[i-1]){
+            sorted=false;
+            break;
         }
     }
-    
-    if (sorted == true)
-    { 
-        cout << "massive is cool" << endl; 
-    }
-    else 
-    { 
-        cout << "massive isnt cool" << endl; 
-    }
+    cout<<endl;
+    if (sorted){cout<<"Sorted!"<<endl;}
+    else {cout<<"Non Sorted!"<<endl;}
     return 0;
 }
