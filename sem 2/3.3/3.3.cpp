ad7241e8c897b6f43414cb45eb9c547a3493cc9b
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 01:19:48 +0500

    Update 3.3.cpp

--- sem 2/3.3/3.3.cpp
+++ sem 2/3.3/3.3.cpp
@@ -2,25 +2,22 @@
 using namespace std;
 int main()
 {
-    const int n = 11;
+    const int N = 11;
     int max;
     int count = 1;
-    int arr[n] = { 487, 452, 6116, 87, 82, 68, 79, 124, 455, 3545, 265 };
-    max = arr[0];
-    cout << arr[0] << " ";
-    for (int i = 1; i < n; i++)
+    int a[N] = { 1, 45, 67, 34, 67, 89, 100, 45, 100, 4, 100 };
+    max = a[0];
+    cout << a[0] << " ";
+    for (int i = 1; i < N; i++)
     {
-        cout << arr[i] << " ";
-        if (arr[i] > max)
+        cout << a[i] << " ";
+        if (a[i] > max)
         {
-            max = arr[i];
+            max = a[i];
             count = 1;
-        }
-        else if (arr[i] == max)
-        {
-            count++;
-        }
-    }
+        }  
+        else if (a[i] == max) { count++; }
+    }  
     cout << endl;
     cout << "Max: " << max << endl;
     cout << "Количество элементов с этим значением: " << count << endl;
