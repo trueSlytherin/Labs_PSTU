f1ea71fc44a3f7f6251a5f2e3363c9a64b4d10c8
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 02:13:53 +0500

    Update 3.10.cpp

--- sem 2/3.10/3.10.cpp
+++ sem 2/3.10/3.10.cpp
@@ -1,27 +1,35 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    const int n = 3;
-
-    int arr[n][n] = { {0, 1, 2}, {1, 2, 3}, {2, 3, 4} };
-
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+using namespace std; 
+int main(){
+    srand(time(0));
+    int range_min = -1000;
+    int range_max = 1000;
+    int N;
+    cin >> N;
+    int a[N][N];
     int tmp;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j<N; j++){
+               a[i][j] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;  
+               cout << a[i][j] << " ";
+        }
+        cout << endl; 
+    }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            tmp = arr[i][j];
-            arr[i][j] = arr[j][i];
-            arr[j][i] = tmp;
+    for (int i = 0; i < N; i ++){
+        for (int j =  i + 1; j < N; j++){
+            tmp = a[i][j];
+            a[i][j] = a[j][i]; 
+            a[j][i] = tmp; 
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << arr[i][j] << " ";
+    for (int i = 0; i < N; i++){
+        for(int j = 0; j<N; j++){
+            cout << a[i][j] << " "; 
         }
         cout << endl;
     }
-
-    return 0;
 }
