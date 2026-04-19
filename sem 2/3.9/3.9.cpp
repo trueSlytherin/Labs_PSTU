f8cb77e7c73f47cea25231bd0542d8781f67a027
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 02:13:33 +0500

    Update 3.9.cpp

--- sem 2/3.9/3.9.cpp
+++ sem 2/3.9/3.9.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+using namespace std; 
+int main(){
+    srand(time(0));
+    int range_min = -100;
+    int range_max = 100; 
+    int N;  
+    cin >> N;
+    int j; 
+    int a[N], b[N];
 
-int main() {
-    const int n = 10;
-    int arr[n] = { 99, 22, 7546, 1, 547, 848, 123, 425, 6234, 63 };
-    int arr_2[n];
-    int j = 0;
+    for (int i = 0; i < N; i++){
+            a[i] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;  
+            cout << a[i] << " ";
+        }
+    cout << endl; 
 
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 == 0) {
-            arr_2[j++] = arr[i];
+    for(int i =0; i< N; i++){
+        if (a[i] % 2 == 0){
+            b[j] = a[i]; 
+            j++;
         }
     }
 
-    for (int i = 0; i < j; i++) {
-        cout << arr_2[i] << " ";
+    for (int i = 0; i < j; i++){
+        cout << b[i] << " ";
     }
-
-    return 0;
 }
