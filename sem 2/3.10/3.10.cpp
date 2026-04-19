2d9ad109711c262ca85e89b5195ba6c1e27a4178
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 02:38:58 +0500

    Update 3.10.cpp

--- sem 2/3.10/3.10.cpp
+++ sem 2/3.10/3.10.cpp
@@ -1,35 +1,30 @@
-#include <iostream> 
-#include <cstdlib>
-#include <ctime>
-using namespace std; 
-int main(){
-    srand(time(0));
-    int range_min = -1000;
-    int range_max = 1000;
-    int N;
-    cin >> N;
-    int a[N][N];
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int n=3;
+	int a[n][n]={ {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
     int tmp;
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j<N; j++){
-               a[i][j] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;  
-               cout << a[i][j] << " ";
-        }
-        cout << endl; 
-    }
-
-    for (int i = 0; i < N; i ++){
-        for (int j =  i + 1; j < N; j++){
-            tmp = a[i][j];
-            a[i][j] = a[j][i]; 
-            a[j][i] = tmp; 
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            tmp=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=tmp;
         }
     }
-
-    for (int i = 0; i < N; i++){
-        for(int j = 0; j<N; j++){
-            cout << a[i][j] << " "; 
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout<<a[i][j]<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
+    cout<<endl;
+    return 0;
 }
