65498a81878c885c1f64bad77bca7d86856986d1
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-20 05:00:28 +0500

    Update 8fersei.cpp

--- sem 2/8fersei/8fersei.cpp
+++ sem 2/8fersei/8fersei.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
 using namespace std;
-const int size_arr=8;
-int a[size_arr][size_arr]{};
-int tryCount=0;
-void show(){
-    for (int i=0; i<size_arr; i++){
-        for (int j=0; j<size_arr; j++){
-            cout<<(a[i][j] ? "! " : "# ");
-        }
-        cout<<endl;
-    }
+
+const int N = 8;
+void vizualization(int a[N][N]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << "|";
+			if (a[i][j] == 1) cout << "Q";
+			else cout << "*";
+		}
+		cout << "|" << endl;
+	}
+	for (int j = 0; j < N; j++) {
+		cout << "--";
+	}
+	cout << "-" << endl;
 }
-bool test(int row, int col){
-    for (int i=0; i<row;i++){
-        if (a[i][col]){return false;}
-    }
-    for (int i=1; row-i>=0 && col-i>=0; i++){
-        if (a[row-i][col-i]){return false;}
-    }
-    for (int i=1; row-i>=0 && col+i<size_arr;i++){
-        if (a[row-i][col+i]){return false;}
-    }
-    return true;
+bool proverka(int a[N][N], int x, int y) {
+	for (int str = 0; str < y; str++) {
+		for (int stolb = 0; stolb < N; stolb++) {
+			if (a[str][stolb] == 1) {
+				if ((x == stolb) || (y == str) || (x - y == stolb - str) || ((N - x) - y == (N - stolb) - str))
+					return 0;	
+			}
+		}
+	} 
+	return 1;
 }
-void game(int row){
-    if (row==size_arr){
-        cout<<"Try №"<<++tryCount<<endl<<endl;
-        show();
-        cout<<endl;
-        return;
-    }
-    for (int col=0; col<size_arr; col++){
-        if (test(row, col)){
-            a[row][col]=1;
-            game(row+1);
-            a[row][col]=0;
-        }
-    }
+
+void queens(int a[N][N], int str) {
+	static int count = 0;
+	for (int stolb = 0; stolb < N; stolb++) {
+		if (proverka(a, stolb, str)) {
+			if (str == N-1) {
+				a[str][stolb] = 1;
+				vizualization(a);
+				a[str][stolb] = 0;
+				count++;
+			}
+			else {
+				a[str][stolb] = 1;
+				queens(a, str + 1);
+				a[str][stolb] = 0;
+			}
+		}
+	}
+	if (str == 0) cout << "total boards: " << count << endl;
+}
+
+
+int main(){
+	int a[N][N];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			a[i][j] = 0;
+		}
+	}
+	queens(a, 0);
 }
 int main(){
     game(0);
