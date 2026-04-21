d9813486c5bdc31b5d1aa4815acd03335aeb1e07
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-22 02:54:09 +0500

    Update lab6.cpp

--- sem 2/lab 6 v6/lab6.cpp
+++ sem 2/lab 6 v6/lab6.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+bool isLetter(char c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+void transformString(char s[]) {
+    char result[256];
+    int len = strlen(s);
+    int k = 0;
+    for (int i = 0; i < len; i++) {
+        if (isLetter(s[i])) {
+            result[k] = s[i];
+            k++;
+        }
+    }
+    for (int i = 0; i < len; i++) {
+        if (isDigit(s[i])) {
+            result[k] = s[i];
+            k++;
+        }
+    }
+    result[k] = '.';
+    k++;
+    result[k] = '\0';
+
+    strcpy_s(s, 256, result);
+}
+
+int main() {
+    char s[256];
+    cout << "Введите строку с точкой в конце:" << endl;
+    cin.getline(s, 256);
+    int len = strlen(s);
+    if (len > 0 && s[len - 1] == '.') {
+        s[len - 1] = '\0';
+    }
+    cout << "Исходная строка: " << s << endl;
+    transformString(s);
+    cout << "Результат: " << s << endl;
+
+    return 0;
+}
