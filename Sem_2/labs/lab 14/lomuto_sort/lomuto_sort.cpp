21bee9aa186bd1d2aab1a6390dd13f382921eabb
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-29 04:51:19 +0500

    Update lomuto_sort.cpp

--- Sem_2/labs/lab 14/lomuto_sort/lomuto_sort.cpp
+++ Sem_2/labs/lab 14/lomuto_sort/lomuto_sort.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+#include <windows.h>
+#include <utility>
 
+using namespace std;
+
+int partitionLomuto(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSortLomuto(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partitionLomuto(arr, low, high);
+        quickSortLomuto(arr, low, pi - 1);
+        quickSortLomuto(arr, pi + 1, high);
+    }
+}
+
+void printArr(int arr[], int n) {
+    cout << "[";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+        if (i < n - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    int arr[] = { 9, 1, 4, 0, 2, 3, 5, 7, 8, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Исходный массив: ";
+    printArr(arr, n);
+
+    quickSortLomuto(arr, 0, n - 1);
+
+    cout << "Отсортированный массив: ";
+    printArr(arr, n);
+
+    return 0;
+}
