f1f0f806096fe3f054a341665ce312f177bcc33f
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-29 02:51:48 +0500

    Update linear_search.cpp

--- Sem_2/labs/lab 12/linear_search/linear_search.cpp
+++ Sem_2/labs/lab 12/linear_search/linear_search.cpp
@@ -1,1 +1,29 @@
-
+# include <iostream>
+# include <Windows.h>
+using namespace std;
+int linearSearch(int arr[], int n, int x) {
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == x) return i;
+	}
+	return -1;
+}
+void printArr(int arr[], int n) {
+	cout << "[";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i];
+		if (i < n - 1) cout << ", ";
+	}
+	cout << "]" << endl;
+}
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	int arr[] = { 42, 3, 56, 22, 77, 11, 1, 8, 43, 84 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int x = 43;
+	cout << "Массив: ";
+	printArr(arr, n);
+	int res = linearSearch(arr, n, x);
+	if (res == -1) cout << "Элемент " << x << " не найден" << endl;
+	else cout << "Элемент " << x << " найден по индексу " << res << endl;
+	return 0;
+}
