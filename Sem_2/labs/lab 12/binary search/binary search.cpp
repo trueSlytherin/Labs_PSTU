6feed5a1f114c8bb58afa968aa81af238c3c1827
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-29 02:54:27 +0500

    Update binary search.cpp

--- Sem_2/labs/lab 12/binary search/binary search.cpp
+++ Sem_2/labs/lab 12/binary search/binary search.cpp
@@ -1,1 +1,71 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int x) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+void printArr(int arr[], int n) {
+    cout << "[";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+        if (i < n - 1)
+            cout << ", ";
+    }
+
+    cout << "]" << endl;
+}
+
+void insertionSort(int arr[], int n) {
+    int key, j;
+
+    for (int i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    int arr[] = { 42, 53, 14, 66, 3, 234, 9, 2, 47 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 53;
+
+    insertionSort(arr, n);
+
+    cout << "Массив:" << endl;
+    printArr(arr, n);
+    cout << endl;
+
+    int res = binarySearch(arr, 0, n - 1, x);
+
+    if (res == -1)
+        cout << "Элемент " << x << " не найден" << endl;
+    else
+        cout << "Элемент " << x << " найден по индексу " << res << endl;
+
+    return 0;
+}
