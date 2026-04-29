a0aa91eed17ce2ad926cd45caf9604335348b0a3
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-29 05:03:12 +0500

    Update shell_sort.cpp

--- Sem_2/labs/lab 15/shell_sort/shell_sort.cpp
+++ Sem_2/labs/lab 15/shell_sort/shell_sort.cpp
@@ -1,1 +1,34 @@
-
+#include <iostream>
+#include <windows.h>
+using namespace std;
+void shellSort(int arr[], int n) {
+	for (int h = n / 2; h > 0; h /= 2) {
+		for (int i = h; i < n; i++) {
+			int tmp = arr[i];
+			int j;
+			for (j = i; j >= h && arr[j - h] > tmp; j -= h) {
+				arr[j] = arr[j - h];
+			}
+			arr[j] = tmp;
+		}
+	}
+}
+void printArray(int arr[], int n) {
+	cout << "[";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i];
+		if (i < n - 1) cout << ", ";
+	}
+	cout << "]" << endl;
+}
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	int arr[] = { 91, 32, 27, 2, 4, 3, 5, 67, 35, 17 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Исходный массив: ";
+	printArray(arr, n);
+	shellSort(arr, n);
+	cout << "Отсортированный массив: ";
+	printArray(arr, n);
+	return 0;
+}
