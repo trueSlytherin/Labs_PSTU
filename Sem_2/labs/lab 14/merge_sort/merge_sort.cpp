3aaaf3c6e69a1e6d812e4027dee6668788856c46
trueSlytherin <116953906+trueSlytherin@users.noreply.github.com>
2026-04-29 11:33:04 +0500

    Update merge_sort.cpp

--- Sem_2/labs/lab 14/merge_sort/merge_sort.cpp
+++ Sem_2/labs/lab 14/merge_sort/merge_sort.cpp
@@ -1,88 +1,65 @@
 #include <iostream>
-#include <ctime>
-
+#include <windows.h>
 using namespace std;
-
-void merge(int* arr, int left, int mid, int right) {
-    int leftSize = mid - left + 1;
-    int rightSize = right - mid;
-
-    int* leftArr = new int[leftSize];
-    int* rightArr = new int[rightSize];
-
-    for (int i = 0; i < leftSize; i++) {
-        leftArr[i] = arr[left + i];
-    }
-
-    for (int i = 0; i < rightSize; i++) {
-        rightArr[i] = arr[mid + 1 + i];
-    }
-
-    int leftIndex = 0;
-    int rightIndex = 0;
-    int mergeIndex = left;
-
-    while (leftIndex < leftSize && rightIndex < rightSize) {
-        if (leftArr[leftIndex] <= rightArr[rightIndex]) {
-            arr[mergeIndex] = leftArr[leftIndex];
-            leftIndex++;
-        }
-        else {
-            arr[mergeIndex] = rightArr[rightIndex];
-            rightIndex++;
-        }
-        mergeIndex++;
-    }
-
-    while (leftIndex < leftSize) {
-        arr[mergeIndex] = leftArr[leftIndex];
-        leftIndex++;
-        mergeIndex++;
-    }
-
-    while (rightIndex < rightSize) {
-        arr[mergeIndex] = rightArr[rightIndex];
-        rightIndex++;
-        mergeIndex++;
-    }
-
-    delete[] leftArr;
-    delete[] rightArr;
+void merge(int arr[], int start, int mid, int end) {
+	int leftRange = mid - start + 1;
+	int rightRange = end - mid;
+	int* leftArr = new int[leftRange];
+	int* rightArr = new int[rightRange];
+	for (int i = 0; i < leftRange; i++) {
+		leftArr[i] = arr[start + i];
+	}
+	for (int j = 0; j < rightRange; j++) {
+		rightArr[j] = arr[mid + 1 + j];
+	}
+	int leftIndex = 0, rightIndex = 0, mergeIndex = start;
+	while (leftIndex < leftRange && rightIndex < rightRange) {
+		if (leftArr[leftIndex] <= rightArr[rightIndex]) {
+			arr[mergeIndex] = leftArr[leftIndex];
+			leftIndex++;
+		}
+		else {
+			arr[mergeIndex] = rightArr[rightIndex];
+			rightIndex++;
+		}
+		mergeIndex++;
+	}
+	while (leftIndex < leftRange) {
+		arr[mergeIndex] = leftArr[leftIndex];
+		leftIndex++;
+		mergeIndex++;
+	}
+	while (rightIndex < rightRange) {
+		arr[mergeIndex] = rightArr[rightIndex];
+		rightIndex++;
+		mergeIndex++;
+	}
+	delete[] leftArr;
+	delete[] rightArr;
 }
-
-void merge_sort(int* arr, int start, int end) {
-    if (start >= end) return;
-
-    int mid = start + (end - start) / 2;
-
-    merge_sort(arr, start, mid);
-    merge_sort(arr, mid + 1, end);
-    merge(arr, start, mid, end);
+void mergeSort(int arr[], int start, int end) {
+	if (start >= end) return;
+	int mid = start + (end - start) / 2;
+	mergeSort(arr, start, mid);
+	mergeSort(arr, mid + 1, end);
+	merge(arr, start, mid, end);
 }
-
+void printArr(int arr[], int n) {
+	cout << "[";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i];
+		if (i < n - 1) cout << ", ";
+	}
+	cout << "]" << endl;
+}
 int main() {
-    srand(time(0));
-
-    int size = 10;
-    int* arr = new int[size];
-
-    cout << "Исходный массив: ";
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 101;
-        cout << arr[i] << " ";
-    }
-
-    cout << endl;
-
-    merge_sort(arr, 0, size - 1);
-
-    cout << "Отсортированный массив: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-
-    cout << endl;
-
-    delete[] arr;
-    return 0;
+	SetConsoleOutputCP(CP_UTF8);
+	int arr[] = { 38, 27, 43, 3, 9, 82, 10, 5, 1, 19 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Исходный массив: ";
+	printArr(arr, n);
+	mergeSort(arr, 0, n - 1);
+	cout << "Отсортированный массив: ";
+	printArr(arr, n);
+	return 0;
 }
